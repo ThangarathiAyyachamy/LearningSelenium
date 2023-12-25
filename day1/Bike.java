a68bb3fc35408f88512e39cd9f5e519eaa42f176
ThangarathiAyyachamy <154514914+ThangarathiAyyachamy@users.noreply.github.com>
2023-12-25 21:25:20 +0100

    Add files via upload

---
+++ day1/Bike.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Car Audi = new Car();
+        Audi.applyBrake();
+        Audi.soundHorn();
+	}
+
+}
